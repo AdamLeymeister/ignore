8c3aed2dbd7bc8de1b743f95bbbd5367265608f7
AdamLeymeister <adam.leymeister@gmail.com>
2023-05-09 23:15:25 -0500

    i

--- d/a/h/nested/Form.jsx
+++ d/a/h/nested/Form.jsx
@@ -1,72 +1,111 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { TabulatorFull as Tabulator } from "tabulator-tables";
 import { tableData } from "./data";
-import {replaceNullValues} from './useReplaceNulls'
+import { replaceNullValues } from './useReplaceNulls'
 import "tabulator-tables/dist/css/tabulator.min.css";
 import "./TestTable.css";
 
 const TestTable = () => {
-  const tableRef = useRef(null);
-  const mockData = replaceNullValues(tableData)
-  console.log(mockData)
+    const [selectedRows, setSelectedRows] = useState([]); // store ids of selected rows
+    const addIsAddedToFriends = (row) => {
+        if (row.friends) {
+          row.friends = row.friends.map(friend => ({ ...friend, isAdded: true }));
+        }
+        return row;
+    };
+    const [data, setData] = useState(replaceNullValues(tableData, undefined, addIsAddedToFriends));
+    const tableRef = useRef(null);
+
   useEffect(() => {
-      const table = new Tabulator(tableRef.current, {
-        data: mockData || [],
-        layout: "fitData",
-        columns: [
-          { title: "ID", field: "id" },
-          { title: "Name", field: "name" },
-          {
-            title: "Friends",
-            field: "friends",
-            formatter: (cell) => {
-              const friends = cell.getValue();
-              const container = friends.reduce((acc, friend) => {
-                const div = document.createElement("div");
-                div.textContent = friend.name;
-                div.classList.add("label");
-                acc.appendChild(div);
-                return acc;
-              }, document.createElement("div"));
-              return container;
-            },
+    const table = new Tabulator(tableRef.current, {
+      data: data || [],
+      layout: "fitData",
+      selectable: true,
+      columns: [
+        { title: "ID", field: "id" },
+        { title: "Name", field: "name" },
+        {
+          title: "Friends",
+          field: "friends",
+          formatter: (cell) => {
+            const friends = cell.getValue();
+            const container = friends.reduce((acc, friend) => {
+              const div = document.createElement("div");
+              div.textContent = friend.name;
+              div.classList.add("label");
+              acc.appendChild(div);
+              return acc;
+            }, document.createElement("div"));
+            return container;
           },
-          {
-            title: "Checkbox",
-            field: "friends",
-            formatter: (cell) => {
-              const friends = cell.getValue();
-              const checkboxes = friends.map((friend) => {
-                const checkbox = document.createElement("input");
-                checkbox.type = "checkbox";
-                checkbox.addEventListener("click", () => {
-                    friend.isAdded = checkbox.checked;
-                  console.log(friend.name + ' isAdded: ' + friend.isAdded);
-
-                });
-
-                const label = document.createElement("label");
-                label.textContent = friend.name;
-
-                return [checkbox, label];
+        },
+        {
+          title: "Checkbox",
+          field: "friends",
+          formatter: (cell, formatterParams, onRendered) => {
+            const friends = cell.getValue();
+            const checkboxes = friends.map((friend) => {
+              const checkbox = document.createElement("input");
+              checkbox.type = "checkbox";
+              checkbox.checked = friend.isAdded || false;
+              checkbox.addEventListener("change", (e) => {
+                e.stopPropagation(); // prevent row click event from firing
+                friend.isAdded = checkbox.checked;
+                setData([...data]); // This triggers a re-render with the updated data
               });
 
-              return checkboxes.flat().reduce((container, element) => {
-                container.appendChild(element);
-                container.classList.add("label");
-                return container;
-              }, document.createElement("div"));
-            },
+              const label = document.createElement("label");
+              label.textContent = friend.name;
+
+              return [checkbox, label];
+            });
+
+            return checkboxes.flat().reduce((container, element) => {
+              container.appendChild(element);
+              container.classList.add("label");
+              return container;
+            }, document.createElement("div"));
           },
-        ],
-      });
+        },
+      ],
+      rowSelected: function(row) { 
+        console.log('2')
+        setSelectedRows([...selectedRows, row.getData().id]);
+      },
+      rowDeselected: function(row) {
+        console.log('3')
+        setSelectedRows(selectedRows.filter(id => id !== row.getData().id));
+      },
+    });
 
-      return () => {
-        table.destroy();
-      };
-  }, [mockData]);
+    // re-select rows after re-render
+    selectedRows.forEach(rowId => {
+        console.log("44")
+      let row = table.getRow(rowId);
+      if (row) {
+        row.select();
+      }
+    });
 
-  return <div ref={tableRef}></div>;
+    return () => {
+      table.destroy();
+    };
+  }, [data, selectedRows]);
+
+  const submitData = async () => {
+    try {
+        console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <div ref={tableRef}></div>
+      <button onClick={submitData}>Submit Data</button>
+    </div>
+  );
 };
 
 export default TestTable;
--- d/a/h/nested/useReplaceNulls.js
+++ d/a/h/nested/useReplaceNulls.js
@@ -1,22 +1,45 @@
-export const replaceNullValues = (data, cache = new Map()) => {
+// This function recursively traverses the provided data, replacing null values with an empty string.
+// It uses a cache to avoid infinite loops when encountering circular references.
+// It also accepts an optional function `fn` that it applies to each object in the data.
+export const replaceNullValues = (data, cache = new Map(), fn = null) => {
+  // If the current data is null, return an empty string.
   if (data === null) {
     return "";
-  } else if (typeof data !== 'object') {
+  } 
+  // If the current data is not an object (it's a primitive value), return it as is.
+  else if (typeof data !== 'object') {
     return data;
-  } else if (Array.isArray(data)) {
-    return data.map(item => replaceNullValues(item, cache));
-  } else {
+  } 
+  // If the current data is an array, map over it, recursively calling replaceNullValues on each item.
+  else if (Array.isArray(data)) {
+    return data.map(item => replaceNullValues(item, cache, fn));
+  } 
+  else {
+    // If the current data is an object that has already been processed, return the cached result.
     if (cache.has(data)) {
       return cache.get(data);
     }
-    const result = Object.entries(data).reduce((acc, [key, value]) => {
-      const replacedValue = replaceNullValues(value, cache);
+  
+    // If the current data is an unprocessed object, reduce over its entries (key-value pairs).
+    // For each entry, recursively call replaceNullValues on the value and add the resulting key-value pair
+    // to the accumulator object (if the resulting value is not null).
+    let result = Object.entries(data).reduce((acc, [key, value]) => {
+      const replacedValue = replaceNullValues(value, cache, fn);
       if (replacedValue !== null) {
         acc[key] = replacedValue;
       }
       return acc;
     }, {});
+
+    // If the optional function parameter was provided, call it with the result object and assign the return value to result.
+    if (fn) {
+      result = fn(result);
+    }
+
+    // Cache the result object using the original data object as the key.
     cache.set(data, result);
+
+    // Return the result object.
     return result;
   }
-}
+};
