0a0ebe6e0f87a88b0383af9ca471ca8ad078a55b
AdamLeymeister <adam.leymeister@gmail.com>
2023-05-24 22:00:03 -0500

    a

---
+++ d/a/h/nested/icons.js
@@ -0,0 +1,121 @@
+import React, { useEffect, useRef, useState } from "react";
+import { TabulatorFull as Tabulator } from "tabulator-tables";
+import { tableData } from "./data";
+import { replaceNullValues } from './useReplaceNulls'
+import "tabulator-tables/dist/css/tabulator.min.css";
+import "./TestTable.css";
+
+const greenCheckSvg = `
+<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 50 50">
+    <polyline points="10 25 20 35 40 15" stroke="green" stroke-width="4" fill="none" />
+</svg>
+`;
+
+const redCrossSvg = `
+<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 50 50">
+    <line x1="15" y1="15" x2="35" y2="35" stroke="red" stroke-width="4"/>
+    <line x1="35" y1="15" x2="15" y2="35" stroke="red" stroke-width="4"/>
+</svg>
+`;
+
+
+const TestTable = () => {
+    const [selectedRows, setSelectedRows] = useState([]); // store ids of selected rows
+    const addIsAddedToFriends = (row) => {
+        if (row.friends) {
+          row.friends = row.friends.map(friend => ({ ...friend, isAdded: true }));
+        }
+        return row;
+    };
+    const [data, setData] = useState(replaceNullValues(tableData, undefined, addIsAddedToFriends));
+    const tableRef = useRef(null);
+
+  useEffect(() => {
+    const table = new Tabulator(tableRef.current, {
+      data: data || [],
+      layout: "fitData",
+      selectable: true,
+      columns: [
+        { title: "ID", field: "id" },
+        { title: "Name", field: "name" },
+        {
+          title: "Friends",
+          field: "friends",
+          formatter: (cell) => {
+            const friends = cell.getValue();
+            const container = friends.reduce((acc, friend) => {
+              const div = document.createElement("div");
+              div.textContent = friend.name;
+              div.classList.add("label");
+              acc.appendChild(div);
+              return acc;
+            }, document.createElement("div"));
+            return container;
+          },
+        },
+        {
+          title: "Checkbox",
+          field: "friends",
+formatter: (cell, formatterParams, onRendered) => {
+  const friends = cell.getValue();
+  const icons = friends.map((friend) => {
+    const div = document.createElement("div");
+    div.innerHTML = friend.isAdded ? greenCheckSvg : redCrossSvg;
+    div.addEventListener("click", (e) => {
+      e.stopPropagation(); // prevent row click event from firing
+      friend.isAdded = !friend.isAdded;
+      setData([...data]); // This triggers a re-render with the updated data
+    });
+    return div;
+  });
+
+  return icons.reduce((container, element) => {
+    container.appendChild(element);
+    container.classList.add("label");
+    return container;
+  }, document.createElement("div"));
+},
+
+        },
+      ],
+      rowSelected: function(row) { 
+        console.log('2')
+        setSelectedRows([...selectedRows, row.getData().id]);
+      },
+      rowDeselected: function(row) {
+        console.log('3')
+        setSelectedRows(selectedRows.filter(id => id !== row.getData().id));
+      },
+    });
+
+    // re-select rows after re-render
+    selectedRows.forEach(rowId => {
+        console.log("44")
+      let row = table.getRow(rowId);
+      if (row) {
+        row.select();
+      }
+    });
+
+    return () => {
+      table.destroy();
+    };
+  }, [data, selectedRows]);
+
+  const submitData = async () => {
+    try {
+        console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <div ref={tableRef}></div>
+      <button onClick={submitData}>Submit Data</button>
+    </div>
+  );
+};
+
+export default TestTable;
