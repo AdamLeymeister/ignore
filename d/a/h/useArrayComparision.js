dbef5fc16938217d574ef3e93c9c2ee698dcd4d8
AdamLeymeister <adam.leymeister@gmail.com>
2023-07-31 00:30:22 -0500

    a

--- d/a/h/useArrayComparision.js
+++ d/a/h/useArrayComparision.js
@@ -1,49 +1,41 @@
 import { useState, useEffect } from 'react';
 
-function useArrayComparison(arrayOne, arrayTwo) {
-  // Declare state variables for the objects not present in each array
-  //let notInArrayOne = arrayTwo.filter(x => !arrayOne.includes(x));
-  //let notInArrayTwo = arrayOne.filter(x => !arrayTwo.includes(x));
-  const [notInArrayOne, setNotInArrayOne] = useState([]);
-  const [notInArrayTwo, setNotInArrayTwo] = useState([]);
+function useArrayComparison(initialState, finalState) {
+  const [addedObjects, setAddedObjects] = useState([]);
+  const [removedObjects, setRemovedObjects] = useState([]);
+  const [updatedObjects, setUpdatedObjects] = useState([]);
 
   useEffect(() => {
-    // Declare maps to keep track of which objects are present in each array
-    const arrayOneObjects = arrayOne.reduce((acc, obj) => {
-      acc.set(obj.id, true);
-      return acc;
-    }, new Map());
+    const initialStateMap = new Map(initialState.map(obj => [obj.id, obj]));
+    const finalStateMap = new Map(finalState.map(obj => [obj.id, obj]));
 
-    const arrayTwoObjects = arrayTwo.reduce((acc, obj) => {
-      acc.set(obj.id, true);
-      return acc;
-    }, new Map());
+    let added = [];
+    let removed = [];
+    let updated = [];
 
-    // Declare arrays to store the objects that are not in arrayOne or arrayTwo, respectively
-    let notInOne = [];
-    let notInTwo = [];
-
-    // Loop through both arrays and check if each object's id property is not present in the other array
-    // If it is not present, add the object to the corresponding notIn array
-    for (let i = 0; i < arrayOne.length; i++) {
-      if (!arrayTwoObjects.has(arrayOne[i].id)) {
-        notInTwo.push(arrayOne[i]);
+    for (const [id, finalObj] of finalStateMap.entries()) {
+      if (!initialStateMap.has(id)) {
+        added.push(finalObj);
+      } else {
+        const initialObj = initialStateMap.get(id);
+        if (JSON.stringify(finalObj) !== JSON.stringify(initialObj)) {
+          updated.push(finalObj);
+        }
       }
     }
 
-    for (let i = 0; i < arrayTwo.length; i++) {
-      if (!arrayOneObjects.has(arrayTwo[i].id)) {
-        notInOne.push(arrayTwo[i]);
+    for (const [id, initialObj] of initialStateMap.entries()) {
+      if (!finalStateMap.has(id)) {
+        removed.push(initialObj);
       }
     }
 
-    // Set the state variables to the respective arrays containing the objects that are not present in each array
-    setNotInArrayOne(notInOne);
-    setNotInArrayTwo(notInTwo);
-  }, [arrayOne, arrayTwo]);
+    setAddedObjects(added);
+    setRemovedObjects(removed);
+    setUpdatedObjects(updated);
+  }, [initialState, finalState]);
 
-  // Return an object containing the state variables
-  return { notInArrayOne, notInArrayTwo };
+  return { addedObjects, removedObjects, updatedObjects };
 }
 
 export default useArrayComparison;
