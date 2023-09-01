35369a89e8b2bbf8fa641e398eb94851a0cda8f4
AdamLeymeister <adam.leymeister@gmail.com>
2023-08-31 21:41:58 -0500

    a

--- d/a/h/testing/PieChart/PieChart.js
+++ d/a/h/testing/PieChart/PieChart.js
@@ -1,84 +1,53 @@
-import React, { useEffect, useRef } from "react";
-import Tabulator from 'tabulator-tables';
-import 'tabulator-tables/dist/css/tabulator.min.css';
+import React from 'react';
+import './App.css';
+import PieChartCommon from './PieChart/PieChartCommon';
 
-const PieChartTable = () => {
-  const tableRef = useRef(null);
+function App() {
+  const [selectedRow, setSelectedRow] = React.useState(null);
 
-  useEffect(() => {
-    // Tabulator Configuration
-    const table = new Tabulator(tableRef.current, {
-      columns: [
-        { title: "Name", field: "name", width: 200 },
-        { title: "Chart", field: "chart", formatter: pieChartFormatter, width: 200 },
-      ],
-      data: [
-        { name: "John", chart: [3, 1, 4, 1, 3,2,2,3,5,2] },
-        { name: "Jane", chart: [5, 2, 1] },
-        { name: "Doe", chart: [2, 3, 4] },
-      ],
-    });
-  }, []);
-
-const pieChartFormatter = (cell) => {
-  const canvas = document.createElement("canvas");
-  canvas.height = 100;
-  canvas.width = 100;
-  const ctx = canvas.getContext("2d");
-
-  // Get data from the cell
-  const data = cell.getValue();
-
-  // Calculate the total value for the pie chart
-  const total = data.reduce((acc, val) => acc + val, 0);
-
-  // Initialize the start angle
-  let startAngle = 0;
-
-  // Predefined list of colors (add more if needed)
-  const colors = [
-    "rgba(255, 99, 132, 0.2)",  // Color for the first value
-    "rgba(255, 255, 0, 0.2)",   // Color for the second value
-    "rgba(0, 0, 255, 0.2)",     // Color for the third value
-    // ... (add more colors here)
-  ];
-
-  // Loop over each data point to draw each slice
-  for (let i = 0; i < data.length; i++) {
-    // Use the color based on the position in the array
-    ctx.fillStyle = colors[i % colors.length];
-    
-    // Begin a new path
-    ctx.beginPath();
-    
-    // Move to the center of the pie
-    ctx.moveTo(50, 50);
-    
-    // Draw the arc
-    ctx.arc(
-      50,
-      50,
-      50,
-      startAngle,
-      startAngle + Math.PI * 2 * (data[i] / total)
-    );
-    
-    // Move back to the center
-    ctx.lineTo(50, 50);
-    
-    // Fill the slice
-    ctx.fill();
-    
-    // Update the start angle for the next slice
-    startAngle += Math.PI * 2 * (data[i] / total);
+  const newRow = {
+    column1: 'John',
+    column2: [{ value: 3, label: "Apples", status:1 }, {stats: 3, value: 2, label: "Bananas" }, { status: 2,value: 4, label: "Cherries" }],
+    column3: [{ value: 2, label: "Apples", status:1 }, {stats: 3, value: 2, label: "Bananas" }, { status: 2,value: 4, label: "Cherries" }],
+    column4: [{ value: 3, label: "Apples", status:1 }, {stats: 3, value: 2, label: "Bananas" }, { status: 2,value: 4, label: "Cherries" }],
+    column5: 'stuff',
+    column6: 'more stuff',
   }
 
-  // Return the canvas element to be rendered in the cell
-  return canvas;
+  const findPieChartColumns = (obj) => {
+  const pieChartColumns = {};
+  // Loop through each property of the object
+  for (const [key, value] of Object.entries(obj)) {
+    // Check if the property is an array
+    if (Array.isArray(value)) {
+      // Check if every item in the array is an object containing 'value' and 'label'
+      if (value.every(item => (typeof item === 'object' && item !== null && 'value' in item && 'label' in item))) {
+        // This is a pie chart column, add to the result object
+        pieChartColumns[key] = value;
+      }
+    }
+  }
+  return pieChartColumns;
 };
 
 
-  return <div ref={tableRef}></div>;
-};
 
-export default PieChartTable;
+  React.useEffect(() => {
+    setSelectedRow(findPieChartColumns(newRow));
+  }, []); 
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {selectedRow && Object.keys(selectedRow).map((key,index) => (
+          <div key={index}>
+            <h3>{key}</h3>
+            <PieChartCommon data={selectedRow[key]} width={100} height={100} />
+          </div>
+        ))}
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ d/a/h/testing/PieChart/PieChartCommon.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import * as d3 from 'd3';
+import PropTypes from 'prop-types';
+import { Popper } from '@mui/base';
+
+const PieChart = ({ data, width = 200, height = 200 }) => {
+  const [arcs, setArcs] = useState([]);
+  const [tooltip, setTooltip] = useState({ show: false, anchorEl: null, text: '' });
+
+  useEffect(() => {
+    const pie = d3.pie().value(d => d.value);
+    setArcs(pie(data));
+  }, [data]);
+
+  const radius = Math.min(width, height) / 2;
+  const arcGenerator = d3.arc().innerRadius(0).outerRadius(radius);
+
+  const color = d3.scaleOrdinal()
+    .domain(data.map(d => d.status))
+    .range(["green", "blue", "red"]);
+
+  const handleMouseOver = (arc, event) => {
+    event.currentTarget.setAttribute('opacity', 0.7);
+    setTooltip({
+      show: true,
+      anchorEl: event.currentTarget,
+      text: `${arc.data.label}: ${arc.data.value}`
+    });
+  };
+
+  const handleMouseOut = (event) => {
+    event.currentTarget.setAttribute('opacity', 1);
+    setTooltip({ show: false, anchorEl: null, text: '' });
+  };
+
+  return (
+    <>
+      <svg width={width} height={height}>
+        <g transform={`translate(${width / 2}, ${height / 2})`}>
+          {arcs.map((arc, index) => (
+            <g key={index}>
+              <path 
+                d={arcGenerator(arc)} 
+                fill={color(arc.data.status)}
+                onMouseOver={(e) => handleMouseOver(arc, e)}
+                onMouseOut={(e) => handleMouseOut(e)}
+              />
+              <text 
+                transform={`translate(${arcGenerator.centroid(arc)})`} 
+                dy="0.35em" 
+                textAnchor="middle"
+                fontSize={(radius / 5)}
+              >
+                {arc.data.label}
+              </text>
+            </g>
+          ))}
+        </g>
+      </svg>
+      <Popper open={tooltip.show} anchorEl={tooltip.anchorEl}>
+        <div style={{ padding: '10px', backgroundColor: 'white', border: '1px solid black' }}>
+          {tooltip.text}
+        </div>
+      </Popper>
+    </>
+  );
+};
+
+PieChart.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.number.isRequired,
+      label: PropTypes.string.isRequired,
+      status: PropTypes.number.isRequired,
+    })
+  ),
+  width: PropTypes.number,
+  height: PropTypes.number,
+};
+
+export default PieChart;
