bea95c1ac17988c10d9b77fdf85e3a9c02c3673c
AdamLeymeister <adam.leymeister@gmail.com>
2023-11-30 01:34:34 -0600

    a

--- d/a/h/testing/Status/TableStatus.js
+++ d/a/h/testing/Status/TableStatus.js
@@ -1,72 +1,157 @@
-import React from "react";
-import { Button } from "@mui/material";
-import CheckCircleIcon from "@mui/icons-material/CheckCircle";
-import CancelIcon from "@mui/icons-material/Cancel";
-import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
-import "./TableStatus.css";
+import React, { useState } from "react";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import InputAdornment from "@mui/material/InputAdornment";
+import TextField from "@mui/material/TextField";
+import SearchIcon from "@mui/icons-material/Search";
+import "./StatusTable.css";
 
-const TableStatus = () => {
-  const apiData = [
-    { type: "wrench", count: 1, status: "one" },
-    { type: "wrench", count: 2, status: "two" },
-    { type: "wrench", count: 3, status: "one" },
-    { type: "hammer", count: 1, status: "one" },
-    { type: "screwdriver", count: 1, status: "two" },
-    { type: "drill", count: 1, status: "three" },
-    { type: "drill", count: 1, status: "three" },
-    { type: "drill", count: 1, status: "three" },
-    { type: "drill", count: 1, status: "three" },
-    { type: "drill", count: 1, status: "three" },
-  ];
-  const getIcon = (status) => {
-    switch (status) {
-      case "one":
-        return (
-          <CheckCircleIcon style={{ color: "#5cc85c", marginRight: "5px" }} />
-        );
-      case "two":
-        return <CancelIcon style={{ color: "#6d1111", marginRight: "5px" }} />;
-      case "three":
-        return (
-          <HelpOutlineIcon style={{ color: "black", marginRight: "5px" }} />
-        );
-      default:
-        return null;
-    }
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
+
+export default function CustomStyledTable() {
+  const [filterTexts, setFilterTexts] = useState({
+    name: "",
+    calories: "",
+    fat: "",
+    carbs: "",
+    protein: "",
+  });
+
+  const handleFilterChange = (event, field) => {
+    setFilterTexts((prevFilterTexts) => ({
+      ...prevFilterTexts,
+      [field]: event.target.value,
+    }));
   };
-  const renderButtons = (status, bgColor) =>
-    apiData
-      .filter((tool) => tool.status === status)
-      .map((tool) => (
-        <Button
-          key={`${tool.type}-${tool.count}`}
-          style={{
-            backgroundColor: bgColor,
-            height: "2rem",
-            display: "flex",
-            color: "black",
-          }}>
-          {getIcon(tool.status)}
-          {tool.type} {tool.count}
-        </Button>
-      ));
+
+  const filteredRows = rows.filter((row) => {
+    //Filter with multiple columns; loop over all filters
+    return Object.keys(filterTexts).every((columnFilter) => {
+      //Check if filter is set
+      if (filterTexts[columnFilter]) {
+        //Return row if match is found in the current column
+        return String(row[columnFilter])
+          .toLowerCase()
+          .includes(filterTexts[columnFilter].toLowerCase());
+      }
+      // If no filter is set for the current column, include the row
+      return true;
+    });
+  });
 
   return (
-    <div className="tableStatusGrid">
-      <div className="column">
-        Bad
-        {renderButtons("two", "red")}
-      </div>
-      <div className="column">
-        Good
-        {renderButtons("one", "green")}
-      </div>
-      <div className="column">
-        Eh
-        {renderButtons("three", "gray")}
-      </div>
+    <div>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="custom-styled table">
+          <TableHead>
+            <TableRow hover>
+              <TableCell>
+                <TextField
+                  fullWidth
+                  label="Filter Dessert"
+                  value={filterTexts.name}
+                  onChange={(event) => handleFilterChange(event, "name")}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <SearchIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </TableCell>
+              <TableCell align="right">
+                <TextField
+                  fullWidth
+                  label="Filter Calories"
+                  value={filterTexts.calories}
+                  onChange={(event) => handleFilterChange(event, "calories")}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <SearchIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </TableCell>
+              <TableCell align="right">
+                <TextField
+                  fullWidth
+                  label="Filter Fat (g)"
+                  value={filterTexts.fat}
+                  onChange={(event) => handleFilterChange(event, "fat")}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <SearchIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </TableCell>
+              <TableCell align="right">
+                <TextField
+                  fullWidth
+                  label="Filter Carbs (g)"
+                  value={filterTexts.carbs}
+                  onChange={(event) => handleFilterChange(event, "carbs")}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <SearchIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </TableCell>
+              <TableCell align="right">
+                <TextField
+                  fullWidth
+                  label="Filter Protein (g)"
+                  value={filterTexts.protein}
+                  onChange={(event) => handleFilterChange(event, "protein")}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <SearchIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {filteredRows.map((row) => (
+              <TableRow key={row.name} hover>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
-};
-
-export default TableStatus;
+}
