ec3ae0c305b78082aaa22f60742bef0cb606ad7a
AdamLeymeister <83959267+AdamLeymeister@users.noreply.github.com>
2024-05-14 12:38:14 -0500

    Update p.jsx

--- p.jsx
+++ p.jsx
@@ -1,39 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { MenuItem, Select, FormControl, CircularProgress } from '@mui/material';
-import axios from 'axios';
+echo '"name": "stuff"' | awk -F'"' '{print $4}'
 
-function ApiSelect() {
-    const [objectOptions, setObjectOptions] = useState([]);
-    const [selectedValue, setSelectedValue] = useState('');
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+echo '"name": "stuff"' | sed -n 's/.*"name": "\([^"]*\).*/\1/p'
 
-    useEffect(() => {
-        setSelectedValue(`${items[0].id}-${items[0].version}`)
-    }, []);
-
-    const handleChange = (event) => {
-        setSelectedValue(event.target.value);
-    };
-
-    return (
-        <FormControl fullWidth>
-            <Select
-                value={selectedValue}
-                onChange={handleChange}
-                displayEmpty
-                renderValue={(selected) => {
-                    const [id, version] = selected.split('-');
-                    const option = objectOptions.find(opt => `${opt.id}-${opt.version}` === selected);
-                    return option ? option.name : 'Select an option';
-                }}
-            >
-                {objectOptions.map(option => (
-                    <MenuItem key={`${option.id}-${option.version}`} value={`${option.id}-${option.version}`}>
-                        {option.name}
-                    </MenuItem>
-                ))}
-            </Select>
-        </FormControl>
-    );
-}
+echo '"name": "stuff"' | grep -oP '(?<="name": ")[^"]*'
