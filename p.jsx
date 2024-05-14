0536f8d030b167d4f15a3e7751ad3e1260cb28eb
AdamLeymeister <83959267+AdamLeymeister@users.noreply.github.com>
2024-05-14 12:53:35 -0500

    Update p.jsx

--- p.jsx
+++ p.jsx
@@ -3,3 +3,12 @@ echo '"name": "stuff"' | awk -F'"' '{print $4}'
 echo '"name": "stuff"' | sed -n 's/.*"name": "\([^"]*\).*/\1/p'
 
 echo '"name": "stuff"' | grep -oP '(?<="name": ")[^"]*'
+
+
+
+str='"name": "stuff"'
+# Remove everything before and including "name": "
+temp=${str#*\"name\": \"}
+# Remove everything after the first closing quote
+result=${temp%%\"*}
+echo "$result"
